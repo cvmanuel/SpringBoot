470ab8eba8ea9a43edf6d9f58f5e9a9154cf5ae6
cvmanuel <jnmanuel.villarreal@gmail.com>
2024-10-29 18:54:45 -0600

    first commit

---
+++ Example/src/test/java/com/manu/Example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
